85436fe07389742331e4ddc78f1852d7ba84ddc6
Casprine Assempah <casprine.001@gmail.com>
2020-03-26 17:37:32 +0000

    feat: added fonts

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import {Platform, StatusBar, StyleSheet, View} from "react-native";
+import {SplashScreen} from "expo";
+import * as Font from "expo-font";
+import {Ionicons} from "@expo/vector-icons";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import useLinking from "./navigation/useLinking";
 
 const Stack = createStackNavigator();
 
@@ -15,7 +15,7 @@ export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
   const [initialNavigationState, setInitialNavigationState] = React.useState();
   const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
+  const {getInitialState} = useLinking(containerRef);
 
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
@@ -29,7 +29,9 @@ export default function App(props) {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
+          Inter: require("./assets/fonts/Inter-Regular.ttf"),
+          Karla: require("./assets/fonts/Karla-Regular.ttf")
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
@@ -48,7 +50,7 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
         <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
           <Stack.Navigator>
             <Stack.Screen name="Root" component={BottomTabNavigator} />
@@ -62,6 +64,6 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
---
+++ todo.md
@@ -0,0 +1,27 @@
+List of screens
+
+- A list of tasks for a collection
+  - Ability to add a task
+  - Ability to delete to edit a task
+  - Ability to archive a task
+- A list of collections - A collection is a group of tasks with a common name
+  - Ability to create a new collection
+  - Ability to edit the name of a collection
+  - Ability to selete a collection
+  - type of collections
+    - todo list
+    - expenses
+- Ability to select either dark or light mode
+- Lock screen with PINCODE
+- Settings
+  - Dark Mode toggle
+  - Set pin
+  - About
+
+---
+
+## Todos
+
+- [ ] Add fonts
+- [ ] Add packages
+- [ ] Add generic components
