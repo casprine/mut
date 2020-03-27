4b5a931e0f41292c240aa989f96b5ca91857c018
Casprine Assempah <casprine.001@gmail.com>
2020-03-27 20:19:50 +0000

    Delete index.js

--- components/FormElements/index.js
+++
@@ -1,4 +0,0 @@
-export { default as VerificationCodeDisplay } from './VerificationCodeDisplay';
-export { default as ManagedKeypad } from './Keypad';
-export { default as TextInput } from './TextInput';
-export { default as Button } from './Button';
